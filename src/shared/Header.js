440c51ca6e0fd17c6c3b273d9bce7230ed80aa90
Alex Goode <alexgoodestudio@gmail.com>
2023-11-10 15:50:01 -0500

    fix banner

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -5,14 +5,18 @@ import image1 from "../images/2.png"
 
 function Header() {
   const style = {
-    background: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)), url(${headerImage})`,
+    background: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.7)), url(${image1})`,
     backgroundPosition: "center",
     backgroundSize: "100% auto",
   };
 
+
   return (
-    <div >
-      <img src={image1} className="w-100"/>
+    <div
+    className="jumbotron jumbotron-fluid text-white border-bottom border-dark pt-0"
+    style={style}
+  >
+      {/* <img src={image1} className="w-100"/> */}
       <HeaderNav />
       <div className="container">
         <h1 className="display-4">Find your next favorite movie!</h1>
